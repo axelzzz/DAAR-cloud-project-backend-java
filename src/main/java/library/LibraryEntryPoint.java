94ac295bc31443ecb69762439e5c4bc265304868
axelzzz <axelzavier@gmail.com>
2020-03-22 13:57:33 +0100

    test deploy

--- src/main/java/library/LibraryEntryPoint.java
+++ src/main/java/library/LibraryEntryPoint.java
@@ -1,6 +1,11 @@
 package library;
 
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.UnknownHostException;
+
 import py4j.GatewayServer;
+import py4j.GatewayServer.GatewayServerBuilder;
 
 public class LibraryEntryPoint {
 
@@ -18,14 +23,21 @@ public class LibraryEntryPoint {
     	
     	long startTime1 = System.nanoTime();
     	
-        GatewayServer gatewayServer = new GatewayServer(new LibraryEntryPoint());
-        
-        long endTime1 = System.nanoTime();
-	    long duration = endTime1-startTime1;
-	    duration = duration / 1000000;
-	    System.out.println("duree : "+duration+"ms");
-	    
-        gatewayServer.start();
-        System.out.println("Gateway Server Started");
+    	try {
+			InetAddress javaAddr = InetAddress.getByName("aqueous-scrubland-31409.herokuapp.com");
+			InetAddress pythonAddr = InetAddress.getByName("cloud-project-backend-django.herokuapp.com");
+			//GatewayServer gatewayServer = new GatewayServer(new LibraryEntryPoint());
+	        GatewayServer gatewayServer = new GatewayServer(new LibraryEntryPoint(), 25333, 25334, javaAddr, pythonAddr, 0, 0, null);
+	       
+	        long endTime1 = System.nanoTime();
+		    long duration = endTime1-startTime1;
+		    duration = duration / 1000000;
+		    System.out.println("duree : "+duration+"ms");
+		    
+	        gatewayServer.start();
+	        System.out.println("Gateway Server Started");
+		} catch (UnknownHostException e) {			
+			e.printStackTrace();
+		}         
     }
 }
